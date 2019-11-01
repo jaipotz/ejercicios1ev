671f07c6bd1942acf13423fabaa29030908b8de6
jaipotz <38020720+jaipotz@users.noreply.github.com>
2019-11-01 14:21:48 +0100

    Segunda Version

--- HelloWorld/src/HelloWorld/HelloWorld.java
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		System.out.println("Encantado");
 		// TODO Auto-generated method stub
 
 	}
